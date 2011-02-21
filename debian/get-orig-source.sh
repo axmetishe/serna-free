4f543b6a4ad316676b6950d85f0ef523ed04de1d
Joachim Breitner <mail@joachim-breitner.de>
2011-02-21 23:22:15 +0530

    Encode hg revision in version

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -2,9 +2,9 @@
 
 # sudo apt-get install mercurial
 
-VERSION=4.3.0
+VERSION=4.3.0.20110221
+REV=a7ce052e3472
 
 # no tag ? Let's use branch:
 hg clone https://bitbucket.org/syntext/serna-free-4-3
-
-GZIP=-9 tar cfz serna-free_$VERSION.orig.tar.gz serna-free-4-3
+GZIP=-9 hg archive -R serna-free-4-3 -r $REV -p serna-free-$VERSION serna-free_$VERSION.orig.tar.gz
